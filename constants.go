84dbe1b8c70704aeea2f367e0aaa90c7f66c776d
aoldershaw <aoldershaw@pivotal.io>
2020-01-01 17:53:35 -0500

    Flag godocs

--- constants.go
+++ constants.go
@@ -2,15 +2,22 @@ package regen
 
 import "strings"
 
+// Flag represents one or more flags applied to a group. Multiple flags can be applied using
+// a bitwise OR |. Refer to: https://golang.org/pkg/regexp/syntax/
 type Flag uint
 
 const (
+	// FlagCaseInsensitive corresponds with flag "i" (case-insensitive)
 	FlagCaseInsensitive Flag = 1 << iota
+	// FlagMultiLine corresponds with flag "m" (multi-line mode: ^ and $ match begin/end line in addition to begin/end text)
 	FlagMultiLine
+	// FlagMatchNewLine corresponds with flag "s" (let . match \n)
 	FlagMatchNewLine
+	// FlagUngreedy corresponds with flag "U" (ungreedy: swap meaning of x* and x*?, x+ and x+?, etc)
 	FlagUngreedy
 )
 
+// String gives the flags' string representation (if all the flags were to be set to true)
 func (f Flag) String() string {
 	var sb strings.Builder
 	if f&FlagCaseInsensitive != 0 {
